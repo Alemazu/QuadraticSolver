8ec2f575dc85a8bde09f6d2233ade881ee01e79f
Alemazu <49592140+Alemazu@users.noreply.github.com>
2021-08-23 17:52:33 -0400

    screwed then fixed ui

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -32,10 +32,14 @@ public class QuadraticSolver extends Application {
         Label l1 = new Label(" x^2 + ");
         Label l2 = new Label(" x + ");
         Label l3 = new Label(" = 0 ");
+        Label l4 = new Label("Output: ");
             	
         TextField aField = new TextField();
         TextField bField = new TextField();
         TextField cField = new TextField();
+        
+        TextField outputField = new TextField();
+        outputField.setEditable(false);
         
         Button solveButton = new Button("Solve");
         
@@ -43,13 +47,20 @@ public class QuadraticSolver extends Application {
         bField.setPrefWidth(30);
         cField.setPrefWidth(30);
         
-        grid.add(aField, 1, 0);
-        grid.add(l1, 2, 0);
-        grid.add(bField, 3, 0);
-        grid.add(l2, 4, 0);
-        grid.add(cField, 5, 0);
-        grid.add(l3, 6, 0);
-        grid.add(solveButton, 1, 2, 3, 3);
+        
+        grid.add(aField, 0, 0);
+        grid.add(l1, 1, 0);
+        grid.add(bField, 2, 0);
+        grid.add(l2, 3, 0);
+        grid.add(cField, 4, 0);
+        grid.add(l3, 5, 0);
+        
+        grid.add(l4, 0, 7, 2, 1);
+        grid.add(outputField, 2, 7, 5, 1);
+        
+        
+        grid.add(solveButton, 0, 2, 2, 1);
+        
         
         grid.setVgap(5);
         
@@ -65,7 +76,7 @@ public class QuadraticSolver extends Application {
                 double x1;
                 double x2;
                 
-                System.out.println(32);
+                System.out.println("");
             	
             }
         };
